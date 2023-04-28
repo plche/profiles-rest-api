66f9e2f1fa9cd6e429d8bd0b81dd29d5575a27da
Percy Che <che.percy.l@gmail.com>
2023-04-28 09:43:31 -0500

    [ADD] post method

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -3,4 +3,4 @@ from rest_framework import serializers
 
 class HelloSerializer(serializers.Serializer):
     """Serializes a name field for testing our APIView"""
-    name = serializers.CharField(max_lenght=10)
+    name = serializers.CharField(max_length=10)
